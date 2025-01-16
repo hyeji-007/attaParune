81f3726a147789cc3a9ba5de7bab1a8c91d7e68b
KimWooJun <wojun74@naver.com>
2025-01-16 17:52:32 +0900

    First Message

---
+++ src/main/java/com/green/attaparune/AttaParuneApplication.java
@@ -0,0 +1,13 @@
+package com.green.attaparune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttaParuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttaParuneApplication.class, args);
+    }
+
+}
