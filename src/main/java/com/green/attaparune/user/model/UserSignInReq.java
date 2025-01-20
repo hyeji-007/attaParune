6e77e6e45946b86d1d9ed120eec0a1607abfabd0
SugiSagong <jumoney1012@gmail.com>
2025-01-20 20:40:28 +0900

    유저 로그인 기능 구현 중

--- src/main/java/com/green/attaparune/user/model/UserSignInReq.java
+++ src/main/java/com/green/attaparune/user/model/UserSignInReq.java
@@ -1,4 +1,18 @@
 package com.green.attaparune.user.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Schema(description = "회원 로그인 정보")
 public class UserSignInReq {
+    @Schema(description = "아이디", example = "test", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String uid;
+    @Schema(description = "비밀번호", example = "1212", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String upw;
 }
